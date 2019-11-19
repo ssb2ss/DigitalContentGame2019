adeb7f382036daa49d4071a4a54b85bd94af6730
ssb2ss <ssb2ss@naver.com>
2019-11-19 12:26:24 +0900

    asdf

--- Framework/GameScene2.cpp
+++ Framework/GameScene2.cpp
@@ -1,11 +1,13 @@
 #include "stdafx.h"
 #include "GameScene2.h"
 
+#include "ExitButton.h"
 
 void GameScene2::Initialize()
 {
 	background = PushBackGameObject(new GameObject(L"resources/sprites/background/map_1.png", Vector2(WIDTH / 2, HEIGHT / 2)));
-	std::cout << "여기까지 만들었습니다" << std::endl;
+	GameObject* clearLogo = PushBackGameObject(new GameObject(L"resources/sprites/clear_logo.png", Vector2(WIDTH / 2, HEIGHT / 2)));
+	ExitButton* exitButton = (ExitButton*)Scene::GetCurrentScene().PushBackGameObject(new ExitButton(200, HEIGHT - 100));
 }
 
 
